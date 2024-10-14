3970a6f7b8a0a309be51393020310908431aa93f
SchoolBoyJoJo <jonathanprb060504@gmail.com>
2024-10-15 06:56:06 +0700

    fix login and regist

--- ecommerce-pbkk/routes/web.php
+++ ecommerce-pbkk/routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TemplateController;
 
 // Route::get('/', function () {
@@ -20,7 +19,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-route::get('/login', [LoginController::class, 'index']);
 
 require __DIR__.'/auth.php';
